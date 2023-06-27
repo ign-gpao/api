7a86c38541e12952acda2e3f2ede52e7aee35a40
Arnaud Birk <arnaud.birk@ign.fr>
2023-06-27 09:06:00 +0200

    update sessions activity

--- middlewares/nodes.js
+++ middlewares/nodes.js
@@ -3,14 +3,20 @@ const debug = require('debug')('nodes');
 
 async function getAllNodes(req, res, next) {
   debug('getAllNodes');
-  await req.client.query('SELECT host, status, count(*) FROM sessions S GROUP BY host, status')
+  await req.client.query('SELECT sessions_host, sessions_status, count(*), max(hms_last_activity) AS hms_last_activity FROM view_sessions GROUP BY sessions_host, sessions_status')
     .then((results) => {
       const dict = {};
       results.rows.forEach((row) => {
-        dict[row.host] = dict[row.host] === undefined ? {
-          host: row.host, closed: 0, active: 0, idle: 0, idle_requested: 0, running: 0,
-        } : dict[row.host];
-        dict[row.host][row.status] = row.count;
+        dict[row.sessions_host] = dict[row.sessions_host] === undefined ? {
+          host: row.sessions_host,
+          closed: 0,
+          active: 0,
+          idle: 0,
+          idle_requested: 0,
+          running: 0,
+          hms_last_activity: row.hms_last_activity,
+        } : dict[row.sessions_host];
+        dict[row.sessions_host][row.sessions_status] = row.count;
       });
       const array = [];
 
